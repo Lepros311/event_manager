bcb73c5f5e36b641fa0fe5ac9dff16dd4880cb1f
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-25 11:01:15 -0400

    Use Google Civic Information webservice to create and output a form letter.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,12 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
 
 puts 'Event Manager Initialized!'
+
+# template_letter = File.read('form_letter.html')
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
 
 # contents = File.read('event_attendees.csv')
 # puts contents
@@ -39,10 +45,65 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    # legislators = civic_info.representative_info_by_address(
+    #   address: zip,
+    #   levels: 'country',
+    #   roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    # )
+    # legislators = legislators.officials
+
+    # # legislator_names = legislators.map do |legislator|
+    # #   legislator.name
+    # # end
+
+    # legislator_names = legislators.map(&:name) # same as commented lines above
+
+    # legislators_string = legislator_names.join(", ")
+
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   # zipcode = row[:zipcode]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  # personal_letter = template_letter.gsub('FIRST_NAME', name)
+  # personal_letter.gsub!('LEGISLATORS', legislators)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+
+  # puts personal_letter
+
+  # puts form_letter
+
+  # puts "\n#{name} #{zipcode} #{legislators}"
 end
