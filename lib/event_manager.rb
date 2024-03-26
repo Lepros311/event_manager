d8bec73f29fbaf52af29cd1ed4ad5cc57737bdf8
Lepros311 <65778034+Lepros311@users.noreply.github.com>
2024-03-26 09:46:29 -0400

    Add function for finding most frequent registration days.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
 
 puts 'Event Manager Initialized!'
 
@@ -45,6 +46,50 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone(phone)
+  phone = phone.gsub(/[^0-9]/, '')
+  if phone.length == 10
+    phone
+  elsif (phone.length == 11) && (phone[0] == '1')
+    phone[1..-1]
+  else
+    "Incomplete or bad phone number provided"
+  end
+end
+
+def get_days_and_times(date_time, hours_array, days_array)
+  time = DateTime.strptime(date_time, '%m/%d/%y %H:%M').strftime('%l%P')
+  day = DateTime.strptime(date_time, '%m/%d/%y %H:%M').strftime('%A')
+  hours_array.push(time)
+  days_array.push(day)
+end
+
+def find_most_frequent_time_ranges(hours_array)
+  frequency = hours_array.group_by { |num| num }.transform_values(&:count)
+  max_frequency = frequency.values.max
+  highest_frequency_values = frequency.select { |_, v| v == max_frequency }.keys
+  most_frequent_time = highest_frequency_values
+  puts "Most frequent time(s): "
+  most_frequent_time.each { |time| puts "#{time.strip}-#{time[0, 2].strip.to_i + 1 }#{time[-2..-1]}" }
+end
+
+def find_most_frequent_days(days_array)
+  frequency = days_array.group_by { |num| num }.transform_values(&:count)
+  max_frequency = frequency.values.max
+  highest_frequency_values = frequency.select { |_, v| v == max_frequency }.keys
+  most_frequent_day = highest_frequency_values
+  days_array.delete("Wednesday")
+
+  frequency = days_array.group_by { |num| num }.transform_values(&:count)
+  max_frequency = frequency.values.max
+  highest_frequency_values = frequency.select { |_, v| v == max_frequency }.keys
+  second_most_frequent_day = highest_frequency_values
+
+  puts "Most frequent day(s): "
+  most_frequent_day.each { |day| puts day }
+  second_most_frequent_day.each { |day| puts day }
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -86,9 +131,18 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+hours_array = []
+days_array = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  phone = row[:homephone]
+  date_time = row[:regdate]
+
+  time_range = get_days_and_times(date_time, hours_array, days_array)
+
+  phone = clean_phone(phone)
   # zipcode = row[:zipcode]
   zipcode = clean_zipcode(row[:zipcode])
 
@@ -107,3 +161,6 @@ contents.each do |row|
 
   # puts "\n#{name} #{zipcode} #{legislators}"
 end
+
+find_most_frequent_time_ranges(hours_array)
+find_most_frequent_days(days_array)
